2a7d98787f00477b2bcb6772855df23d73bbe3a2
SHRUTI GUPTA <156588272+Shrutigupta102005@users.noreply.github.com>
2025-09-03 11:35:10 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (77.62%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        return robber(nums, 0, dp);
+    }
+
+    public static int robber(int[] arr, int i, int[] dp) {
+        if (i >= arr.length) {
+            return 0;
+        }
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+        int rob = arr[i] + robber(arr, i + 2, dp);
+        int dontRob = robber(arr, i + 1, dp);
+
+        return dp[i] = Math.max(rob, dontRob);
+    }
+}
