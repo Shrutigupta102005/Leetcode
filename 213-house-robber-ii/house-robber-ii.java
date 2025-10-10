882271295abcba55320be659bb90b5835820459d
SHRUTI GUPTA <156588272+Shrutigupta102005@users.noreply.github.com>
2025-10-10 23:10:54 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (99.69%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,18 +1,22 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 1) return nums[0];
-        return Math.max(getMax(nums, 0, nums.length - 2), getMax(nums, 1, nums.length - 1));        
+        int [] dp1 = new int[nums.length];
+        int [] dp2 = new int[nums.length]; 
+        Arrays.fill(dp1,-1);
+        Arrays.fill(dp2,-1);
+        int c1 = house(nums,0,nums.length-2,dp1);
+        int c2 = house(nums,1,nums.length-1,dp2);
+        return Math.max(c1,c2);
     }
+    public static int house(int[] nums, int idx,int end,int [] dp){
+            if (nums.length == 1) return nums[0];
 
-    private int getMax(int[] nums, int start, int end) {
-        int prevRob = 0, maxRob = 0;
-
-        for (int i = start; i <= end; i++) {
-            int temp = Math.max(maxRob, prevRob + nums[i]);
-            prevRob = maxRob;
-            maxRob = temp;
-        }
-
-        return maxRob;
-    }    
+            if(idx >end){return 0;}
+            if(dp[idx] != -1){
+                return dp[idx];
+            }
+            int f1 = nums[idx]+house(nums,idx+2,end,dp);
+            int f2 = house(nums,idx+1,end,dp);
+            return dp[idx] = Math.max(f1,f2);
+    }
 }
