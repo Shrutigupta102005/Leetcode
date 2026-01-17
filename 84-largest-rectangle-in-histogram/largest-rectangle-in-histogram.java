9d93275a12c11d44136f81a80ad10266e6c916be
SHRUTI GUPTA <156588272+Shrutigupta102005@users.noreply.github.com>
2026-01-17 22:24:43 +0530

    Time: 113 ms (12.03%) | Memory: 77.6 MB (73.68%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,20 +1,50 @@
+import java.util.*;
+
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int n = heights.length;
-        Stack<Integer> st = new Stack<>();
-        int maxArea = 0;
 
-        for (int i = 0; i <= n; i++) {
-            int h = (i == n ? 0 : heights[i]); 
-            while (!st.isEmpty() && heights[st.peek()] > h) {
-                int height = heights[st.pop()];
-                int right = i;
-                int left = st.isEmpty() ? -1 : st.peek();
-                int width = right - left - 1;
-                maxArea = Math.max(maxArea, height * width);
+        int n = heights.length;
+        int[] right = new int[n];
+        int[] left  = new int[n];
+
+        rightSmaller(heights, right);
+        leftSmaller(heights, left);
+
+        int area = 0;
+
+        for (int i = 0; i < n; i++) {
+            int width = right[i] - left[i] - 1;
+            int curr_area = heights[i] * width;
+            area = Math.max(area, curr_area);
+        }
+        return area;
+    }
+
+    // next smaller element index
+    public void rightSmaller(int[] heights, int[] right) {
+        Stack<Integer> st = new Stack<>();
+        int n = heights.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
+                st.pop();
             }
+            right[i] = st.isEmpty() ? n : st.peek();
             st.push(i);
         }
-        return maxArea;
+    }
+
+    // previous smaller element index
+    public void leftSmaller(int[] heights, int[] left) {
+        Stack<Integer> st = new Stack<>();
+        int n = heights.length;
+
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
+                st.pop();
+            }
+            left[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
     }
 }
