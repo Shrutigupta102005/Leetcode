f887c16425304ce12a25c5227b14339b46a4a8e8
SHRUTI GUPTA <156588272+Shrutigupta102005@users.noreply.github.com>
2026-01-18 12:26:43 +0530

    Time: 39 ms (67.72%) | Memory: 98.8 MB (48.36%) - LeetSync

--- 456-132-pattern/132-pattern.java
+++ 456-132-pattern/132-pattern.java
@@ -3,14 +3,14 @@ class Solution {
         Stack<Integer> st = new Stack<>();
         int third = Integer.MIN_VALUE;
         for(int i = nums.length-1;i>=0;i--){
-            if(nums[i] < third){
+            if(nums[i]<third){
                 return true;
             }
             while(!st.isEmpty() && st.peek() < nums[i]){
                 third = st.pop();
             }
             st.push(nums[i]);
-        }   
+        }
         return false;
     }
 }
