62a17f103e6cc9a1a84139b588cd95029b7697df
SHRUTI GUPTA <156588272+Shrutigupta102005@users.noreply.github.com>
2026-01-20 08:31:08 +0530

    Time: 4 ms (73.84%) | Memory: 45.7 MB (7.73%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,23 +1,36 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        ArrayList<Integer> nge = NGE(nums2);
 
-        int [] res = new int[nums1.length];
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums2.length; i++) {
+            map.put(nums2[i], nge.get(i));
+        }
+
+        
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.get(nums1[i]);
+        }
+
+        return ans;
+    }
+    public static ArrayList<Integer> NGE(int [] arr){
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        for(int i= 0 ;i<arr.length;i++){
+            ans.add(-1);
+        }
         Stack<Integer> st = new Stack<>();
-        int [] ans = new int[nums2.length];
-        for(int i = nums2.length-1 ;i>=0;i--){
-            while(!st.isEmpty() && st.peek()<= nums2[i] ){
+        for(int i = arr.length-1;i>=0;i--){
+            while(!st.isEmpty() && arr[i]>= st.peek()){
                 st.pop();
             }
-            ans[i] = st.isEmpty() ? -1 : st.peek();
-            st.push(nums2[i]);
-        }
-        for(int j = 0 ;j< nums1.length ;j++){
-            for(int k = 0 ;k<nums2.length ;k++){
-                if(nums1[j] == nums2[k]){
-                    res[j] = ans[k];
-                }
+            if(!st.isEmpty()){
+            ans.set(i,st.peek());
             }
+            st.push(arr[i]);
         }
-        return res;
+        return ans;
     }
 }
