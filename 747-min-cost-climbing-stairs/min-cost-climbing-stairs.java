b0ae64f1dc4cf3194307e37453483e1e9bfd80ab
SHRUTI GUPTA <156588272+Shrutigupta102005@users.noreply.github.com>
2026-01-21 21:18:27 +0530

    Time: 1 ms (38.22%) | Memory: 45.1 MB (30.60%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,19 +1,22 @@
 class Solution {
-    public int minCostClimbingStairs(int[] cost) {
-        int[] dp = new int[cost.length];
-        Arrays.fill(dp,-1);
-        return Math.min(fun(cost, 0, dp), fun(cost, 1, dp));
+    public int minCostClimbingStairs(int[] cost) {  
+        int[] dp1 = new int[cost.length];
+        Arrays.fill(dp1,-1);
+        int[] dp2 = new int[cost.length];
+        Arrays.fill(dp2,-1);
+         int m1 = mincost(cost,dp1,0);
+         int m2 = mincost(cost,dp2,1);
+        return Math.min(m1,m2);
     }
-    public static int fun(int[] cost,int idx ,int[] dp){
-        if(idx>= cost.length){
+    public int mincost(int[] cost , int[] dp ,int idx){
+        if(idx >= cost.length){
             return 0 ;
         }
-        if(dp[idx] !=  -1){
+        if(dp[idx] != -1){
             return dp[idx];
         }
-        int f1 = cost[idx] + fun(cost ,idx+1 ,dp); 
-        int f2 = cost[idx] + fun(cost ,idx+2 ,dp); 
-        return dp[idx] = Math.min(f1,f2);
+        int pick = cost[idx] + mincost(cost ,dp,idx+1);
+        int notpick = cost[idx] + mincost(cost ,dp,idx+2);
+        return dp[idx] = Math.min(pick,notpick);
     }
-
 }
