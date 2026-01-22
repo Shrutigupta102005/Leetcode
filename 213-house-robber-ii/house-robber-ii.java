3dc88b54aa4a0840dd4a4db2f6a81fc002ca3a4c
SHRUTI GUPTA <156588272+Shrutigupta102005@users.noreply.github.com>
2026-01-22 12:21:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (49.37%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,22 +1,23 @@
 class Solution {
     public int rob(int[] nums) {
-        int [] dp1 = new int[nums.length];
-        int [] dp2 = new int[nums.length]; 
-        Arrays.fill(dp1,-1);
-        Arrays.fill(dp2,-1);
-        int c1 = house(nums,0,nums.length-2,dp1);
-        int c2 = house(nums,1,nums.length-1,dp2);
-        return Math.max(c1,c2);
+        int[] dp1 = new int[nums.length];
+        int[] dp2 = new int[nums.length];
+        Arrays.fill(dp1, -1);
+        Arrays.fill(dp2, -1);
+        int m1 = robb(nums,dp1,0,nums.length-2);
+        int m2 = robb(nums,dp2,1,nums.length-1);
+        return Math.max(m1,m2);
     }
-    public static int house(int[] nums, int idx,int end,int [] dp){
-            if (nums.length == 1) return nums[0];
-
-            if(idx >end){return 0;}
-            if(dp[idx] != -1){
-                return dp[idx];
-            }
-            int f1 = nums[idx]+house(nums,idx+2,end,dp);
-            int f2 = house(nums,idx+1,end,dp);
-            return dp[idx] = Math.max(f1,f2);
+    public int robb(int[]num ,int[]dp,int idx,int end){     
+        if (num.length == 1) return num[0];
+        if(idx > end){
+            return 0 ;
+        }
+        if(dp[idx] != -1){
+            return dp[idx];
+        }
+        int pick = num[idx] + robb(num,dp,idx+2,end);
+        int notpick = robb(num,dp,idx+1,end);
+        return dp[idx] = Math.max(pick,notpick);  
     }
 }
