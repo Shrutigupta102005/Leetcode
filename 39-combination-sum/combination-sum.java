6cf19ce3f26d95590cf653b8466a4d483eacc809
SHRUTI GUPTA <156588272+Shrutigupta102005@users.noreply.github.com>
2026-03-05 01:16:56 +0530

    Time: 2 ms (95.38%) | Memory: 45.7 MB (68.35%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,21 +1,22 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-     List<List<Integer>> ans = new ArrayList<>();
-     combsum(candidates,target,0,new ArrayList<>(),ans);
-     return ans;
+        List<Integer> temp = new ArrayList<>();
+        comb(candidates,target,0,0,temp);
+        return ans;
     }
-    public static void combsum(int[] can, int target, int idx ,List<Integer> curr, List<List<Integer>> ans){
-        if(target == 0){
-            ans.add(new ArrayList<>(curr));
+    List<List<Integer>> ans = new ArrayList<>();
+    public void comb(int[] arr , int target , int start ,int currSum,List<Integer> temp){
+        if(currSum == target){
+            ans.add(new ArrayList<>(temp));
             return;
         }
-        for(int i = idx;i<can.length;i++){
-            if(can[i]<= target){
-                curr.add(can[i]); 
-                combsum(can,target-can[i],i,curr,ans);
-                curr.remove(curr.size() - 1); // backtrack
-
-            }
+        if(currSum > target){
+            return;
+        }
+        for(int i = start;i<arr.length;i++){
+            temp.add(arr[i]);
+            comb(arr,target,i,currSum+arr[i],temp);
+            temp.remove(temp.size()-1);
         }
     }
 }
