b202568b243d0622cc8f227ff8f4f8d73e253336
SHRUTI GUPTA <156588272+Shrutigupta102005@users.noreply.github.com>
2026-03-05 00:57:39 +0530

    Time: 17 ms (88.49%) | Memory: 97.9 MB (84.26%) - LeetSync

---
+++ 77-combinations/combinations.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<Integer> temp = new ArrayList<>();
+        comb(n,k,1,temp);
+        return ans;
+    }
+    List<List<Integer>> ans = new ArrayList<>();
+    public void comb(int n , int k ,int start,List<Integer> temp){
+        if(k == 0){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int i = start;i<=n;i++){
+            temp.add(i);
+            comb(n,k-1,i+1,temp);
+            temp.remove(temp.size()-1);
+        }
+
+    }
+}
