3e483b48ec94e49be86bffb387aafc7aa6d51529
SHRUTI GUPTA <156588272+Shrutigupta102005@users.noreply.github.com>
2026-03-06 11:09:38 +0530

    Time: 2 ms (63.07%) | Memory: 43.2 MB (53.88%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,10 +1,20 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        char c = 0;
-        for(char ch:s.toCharArray()){
-            c ^= ch;
+        char[] f = s.toCharArray();
+        char[] n = t.toCharArray();
+
+        int[]freq=  new int[26];
+        for(char ch : n){
+            freq[ch -'a']++;
         }
-         for(char ct : t.toCharArray()) c ^= ct;
-        return c;
+        for(char ch : f){
+            freq[ch -'a']--;
+        }
+        for(int i=0;i<freq.length;i++){
+            if(freq[i] == 1){
+                return (char) (i+'a');
+            }
+        }
+        return ' ';
     }
 }
