aa70fac3f47fa4eac7adc69be4e7d823e7fdf908
SHRUTI GUPTA <156588272+Shrutigupta102005@users.noreply.github.com>
2026-03-06 10:47:46 +0530

    Time: 14 ms (92.67%) | Memory: 43.4 MB (89.81%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,25 +1,25 @@
-public class Solution {
-private int lo, maxLen;
+class Solution {
+    public String longestPalindrome(String s) {
+        int start = 0 ;
+        int end = 0 ;
 
-public String longestPalindrome(String s) {
-	int len = s.length();
-	if (len < 2)
-		return s;
-	
-    for (int i = 0; i < len-1; i++) {
-     	extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
-     	extendPalindrome(s, i, i+1); //assume even length.
+        for(int i =  0;i<s.length();i++){
+            int odd = expand(s,i,i);
+            int even = expand(s,i,i+1);
+            int len = Math.max(odd,even);        
+                if( len > end-start){
+                    start = i - (len-1)/2;
+                    end = i + len/2;
+                 }
+        }
+             return s.substring(start,end+1);
     }
-    return s.substring(lo, lo + maxLen);
-}
+    public int expand(String s, int left, int right){
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
 
-private void extendPalindrome(String s, int j, int k) {
-	while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
-		j--;
-		k++;
-	}
-	if (maxLen < k - j - 1) {
-		lo = j + 1;
-		maxLen = k - j - 1;
-	}
-}}
+}
