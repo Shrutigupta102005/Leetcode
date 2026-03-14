0c5963af6069557cc0ab676029998367365f0b13
SHRUTI GUPTA <156588272+Shrutigupta102005@users.noreply.github.com>
2026-03-14 23:22:09 +0530

    Time: 5 ms (97.56%) | Memory: 42.9 MB (67.24%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,31 +1,20 @@
 class Solution {
-
     public int countSubstrings(String s) {
-
-        int count = 0;
-
-        for(int i = 0; i < s.length(); i++){
-
-            count += expand(s, i, i);     // odd length
-            count += expand(s, i, i+1);   // even length
+        int count = 0 ;
+        for(int i = 0 ;i<s.length();i++){
+        count += expand(s,i,i);
+        count += expand(s,i,i+1);
         }
-
-        return count;
+        return count ;
     }
 
-    public int expand(String s, int left, int right){
-
-        int count = 0;
-
-        while(left >= 0 && right < s.length() &&
-              s.charAt(left) == s.charAt(right)){
-
+    public int expand(String s, int left , int right){
+        int count=0;
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
             count++;
-
             left--;
             right++;
         }
-
         return count;
     }
 }
