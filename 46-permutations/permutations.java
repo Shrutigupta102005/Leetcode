caa93175a8bdd38d67efea22578641b13c07b859
SHRUTI GUPTA <156588272+Shrutigupta102005@users.noreply.github.com>
2026-04-08 08:31:30 +0530

    Time: 1 ms (85.09%) | Memory: 45.2 MB (96.21%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,25 +1,24 @@
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {     
-        boolean[] used = new boolean[nums.length];
-        perm(nums, new ArrayList<>(), used);
+    public List<List<Integer>> permute(int[] nums) {
+        List<Integer> temp = new ArrayList<>();
+        boolean [] used = new boolean[nums.length];
+        per(nums,used,temp);
         return ans;
+        
     }
     List<List<Integer>> ans = new ArrayList<>();
-
-    public void perm(int[]nums,List<Integer> temp , boolean[] used){
-        if(temp.size() == nums.length){
+    public void per(int[] nums,boolean[] used , List<Integer>temp){
+        if(nums.length == temp.size()){
             ans.add(new ArrayList<>(temp));
             return;
         }
-
         for(int i = 0 ;i<nums.length;i++){
             if(used[i]) continue;
             used[i] = true;
             temp.add(nums[i]);
-            perm(nums,temp,used);
+            per(nums,used,temp);
             temp.remove(temp.size() -1);
-            used[i] = false;
+            used[i] =false;
         }
-
     }
 }
